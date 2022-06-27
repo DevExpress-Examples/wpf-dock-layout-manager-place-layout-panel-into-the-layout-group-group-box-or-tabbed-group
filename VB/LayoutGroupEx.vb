afb671dd288a5e50a6bc71f2c9e9c8df5775e574
DevExpressExampleBot <fakeEmail>
2022-06-27 11:52:01 +0400

    Source auto update [skip ci]

--- VB/LayoutGroupEx.vb
+++ VB/LayoutGroupEx.vb
@@ -1,15 +1,12 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Docking
 
 Namespace CustomLayoutGroup.Common
-	Public Class LayoutGroupEx
-		Inherits LayoutGroup
 
-		Protected Overrides Function CoerceGroupBorderStyle(ByVal value As GroupBorderStyle) As GroupBorderStyle
-			Return value
-		End Function
-	End Class
+    Public Class LayoutGroupEx
+        Inherits LayoutGroup
+
+        Protected Overrides Function CoerceGroupBorderStyle(ByVal value As GroupBorderStyle) As GroupBorderStyle
+            Return value
+        End Function
+    End Class
 End Namespace
